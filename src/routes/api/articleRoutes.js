4a92580e9743bf8f26d1f8cae217a32796efc74c
Gabin Ishimwe <g.ishimwe@alustudent.com>
2022-01-31 11:00:33 +0200

    ft-added feedback

--- src/routes/api/articleRoutes.js
+++ src/routes/api/articleRoutes.js
@@ -7,37 +7,20 @@ const route = express.Router()
 const storageFile = multer.diskStorage({}) // take none because image will be upload on cloudinary no locally
 const upload = multer({storage: storageFile, file: fileFilter})
 
-route.post('/create', upload.single("image"), async (req, res, next) => {
-    console.log("route run")
-    await ArticleController.createArticle(req, res)
-})
+route.post('/create', upload.single("image"),  ArticleController.createArticle)
 
-route.get("/all", async (req, res, next) => {
-    await ArticleController.getAllArticles(req, res)
-})
+route.get("/all", ArticleController.getAllArticles)
 
-route.get("/:id", async (req, res) => {
-    await ArticleController.getArticle(req, res)
-})
+route.get("/:id", ArticleController.getArticle)
 
-route.patch("/:id", upload.single("image"),async (req, res) => {
-    await ArticleController.updateArticle(req, res)
-})
+route.patch("/:id", upload.single("image"), ArticleController.updateArticle)
 
-route.delete("/:id", async (req, res) => {
-    await ArticleController.deleteArticle(req, res)
-})
+route.delete("/:id", ArticleController.deleteArticle)
 
-route.post("/:id/addComment", async (req, res) => {
-    await CommentController.createComment(req, res)
-})
+route.post("/:id/addComment", CommentController.createComment)
 
-route.get("/:id/deleteComments", async (req, res) => {
-    await CommentController.deleteComments(req, res)
-})
+route.get("/:id/deleteComments", CommentController.deleteComments)
 
-route.get("/:id/deleteOneComment/:commentNum", async (req, res) => {
-    await CommentController.deleteOneComment(req, res)
-})
+route.get("/:id/deleteOneComment/:commentNum", CommentController.deleteOneComment)
 
 export default route
--- src/routes/api/queriesRoutes.js
+++ src/routes/api/queriesRoutes.js
@@ -3,21 +3,13 @@ import QueryController from '../../controllers/queriesController.js'
 
 const route = express.Router()
 
-route.get('/getAll', async (req, res, next) => {
-    await QueryController.getAllQueries(req, res)
-})
+route.get('/getAll', QueryController.getAllQueries)
 
-route.post('/createQuerie', async (req, res, next) => {
-    await QueryController.creatingQuery(req, res)
-})
+route.post('/createQuerie', QueryController.creatingQuery)
 
-route.delete("/delete/:id", async (req, res) => {
-    await QueryController.deleteQueries(req, res)
-})
+route.delete("/delete/:id", QueryController.deleteQueries)
 
-route.get("/getOne/:id", async (req, res) => {
-    await QueryController.getOneQuery(req, res)
-})
+route.get("/getOne/:id", QueryController.getOneQuery)
 
 export default route
 

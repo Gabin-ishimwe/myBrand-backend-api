180081cc6c54b31d0b422497de377d32922ef369
Gabin Ishimwe <g.ishimwe@alustudent.com>
2022-02-10 14:55:51 +0200

    added another test

--- tests/articleTest.js
+++ tests/articleTest.js
@@ -119,11 +119,12 @@ describe("ARTICLES", () => {
 
 
           it("it should create an article", async ()=> {
+               console.log(__dirname)
                const token = await generateToken({id: testSchema._id})
                const res = await chai.request(app).post("/api/v1/articles/")
                .set({"Authorization": `Bearer ${token}`})
                // .type("form")
-               .attach("image", path.join( __dirname, "/testingImage/linkedinAboutMe.png"), "linkedinAboutMe.png")
+               .attach("image", path.join( __dirname, "/testingImage/linkedinAboutMe.PNG"), "linkedinAboutMe.png")
                .field("title", "Creating article")
                .field("content", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
                .field("likes", 0)
